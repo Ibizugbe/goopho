70c3d0de3d997f5ffc560368b5b797f82642db44
ibizugbe <abrahamibizugbe@gmail.com>
2021-12-22 10:51:41 +0100

    init

---
+++ hello.py
@@ -0,0 +1,44 @@
+# # #an operation that adds two numbers.
+# # first_number = input("input first number: ")
+# # second_number = input("input second number: ")
+# # add_operator = int(first_number) + int(second_number)
+# # print(add_operator)
+# print(1 > 2)
+
+
+# TO FIMD THE AVERAGE OF N NUMBERS
+# temperature = int(input("Temperature: "))
+# if temperature > 30:
+#   print("It's hot outside")
+#   print("deink water!!")
+# elif temperature > 20:
+#   print("The weather condition is okay.")
+# else:
+#   print("it's cold outside")
+# print("done")
+# temperature = 32
+# if temperature > 30:
+#   message = "its hot, drink water!"
+# elif temperature > 20:
+#   message = "its cold!"
+# else:
+#   message = "the weather is fine"
+# print(message)
+# temperature = 32
+# message = "its hot, drink water!" if temperature >=30 else "the weather is fine"
+# print(message)
+###logical operators  AND OR NOT
+
+#LOAN ELIGIBILITY
+# good_credit = True
+# high_income = False
+# student = True
+# if (good_credit and high_income) and not student:
+#   print("very eligible")
+# elif (good_credit or high_income) and not student:
+#   print("eligible")
+# else:
+#   print("not eligible")
+# short circuit
+#chaining comparison operators
+#
